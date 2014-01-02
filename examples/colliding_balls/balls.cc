2ed3471f6db810d3e351dd94d715113136fb6da2
Julien Bernard <julien.bernard@univ-fcomte.fr>
2014-01-02 17:46:14 +0100

    update copyright year

--- examples/colliding_balls/balls.cc
+++ examples/colliding_balls/balls.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Julien Bernard
+ * Copyright (c) 2013-2014, Julien Bernard
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
