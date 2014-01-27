36f4a1e3b2ddcb74fa5b4a965d491a77f5a61536
Julien Bernard <julien.bernard@univ-fcomte.fr>
2014-01-27 22:46:40 +0100

    replace constexpr by inline for Hash with std::string

--- src/include/es/Type.h
+++ src/include/es/Type.h
@@ -35,7 +35,7 @@ namespace es {
     return sz == 0 ? 0xcbf29ce484222325 : (str[0] ^ Hash(str + 1, sz - 1)) * 0x100000001b3;
   }
 
-  constexpr Type Hash(const std::string& str) {
+  inline Type Hash(const std::string& str) {
     return Hash(str.c_str(), str.size());
   }
 
